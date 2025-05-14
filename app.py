37c3cbc83ac0d239e220e76d9f8d51b7568e6be6
TUMTUMSAUR <marinarogiorgio23@gmail.com>
2025-05-14 17:48:08 +0200

    log

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from threading import Thread
 app = Flask('')
 
 html_code = """
+<!DOCTYPE html>
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
